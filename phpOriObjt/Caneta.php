8e1d48ee43785b4af537765e985a3d098dc48dfc
Anderson Mattar <andmat7745@gmail.com>
2023-11-22 17:16:18 -0300

    aula02

---
+++ phpOriObjt/Caneta.php
@@ -0,0 +1,22 @@
+<?php
+class Caneta{
+    var $modelo;
+    var $cor;
+    var $ponta;
+    var $carga;
+    var $tampada;
+    function rabiscar(){
+        if ($this->tampada == true) {
+            echo "Erro não posso rabiscar...";
+        } else {
+            echo "<p>Estou rabiscando!</p>";
+        }
+        
+    }
+    function tampar(){
+
+    }
+    function destampar(){
+
+    }
+}
